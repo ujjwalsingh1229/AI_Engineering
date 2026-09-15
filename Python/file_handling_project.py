c151a7bd280fb1dee358bd38b83b8e2f3cdc9d95
Ujjwal Singh <ujjawalsingh1221@gmail.com>
2026-09-15 16:48:40 +0530

    updation

--- Python/file_handling_project.py
+++ Python/file_handling_project.py
@@ -1,6 +1,7 @@
 # CRUD OPERATION PROJECT ON FILE HANDLING
 
 from pathlib import Path
+import os 
 
 def readfileandfolder():
     path = Path('')
@@ -41,6 +42,53 @@ def readfile():
     except Exception as err:
         print(f"An error occured as {err}")
 
+def updatefile():
+    try:
+        readfileandfolder()
+        name = input("tell which file you want to update : ")
+        p = Path(name)
+        if p.exists() and p.is_file():
+                print("press 1 for changing the name of your file : ")
+                print("press 2 for overwriting the data of your file ")
+                print("press 3 for appending some content in your life")
+        
+                res = int(input("tell your response : "))
+        
+                if res == 1 :
+                    name2 = input("tell your new file name : ")
+                    p2 = Path(name2)
+                    p.rename(p2)
+        
+                if res == 2:
+                    with open(p,'w') as fs:
+                      data = input("tell me what you want to write this is overwrite the data : ")
+                      fs.write(data)
+        
+                if res == 3:
+                    with open(p,'a') as fs:
+                        data = input("tell me what you want to append : ")
+                        fs.write(data)
+    except Exception as err: 
+        print("an error occurred") 
+
+def deletefile():
+    try:
+        readfileandfolder()
+        name = input("which file you want to delete : ")
+        p = Path(name)
+
+        if p.exists() and p.is_file():
+            os.remove(name)
+
+            print("file removes successfully")
+        else:
+            print("No such file exist")
+
+    except Exception as err:
+        print(f"An error occured as {err}")           
+
+
+
 print("press 1 for creating a file")
 print("press 2 for reading a file")
 print("press 3 for updating a file")
@@ -53,3 +101,9 @@ if check == 1:
 
 if check == 2:
     readfile()
+
+if check == 3:
+    updatefile()
+
+if check == 4:
+    deletefile()
